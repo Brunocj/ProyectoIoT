207ee7d4edc09e275d17fc39fd2c401f62970229
Brunocj <a20212474@pucp.edu.pe>
2025-09-10 17:21:03 -0500

    fragment buscar

--- app/src/main/java/com/example/appventure/Usuario/HomeFragmentUsuario.java
+++ app/src/main/java/com/example/appventure/Usuario/HomeFragmentUsuario.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,5 +22,21 @@ public class HomeFragmentUsuario extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_usuario_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView verTodo = view.findViewById(R.id.tvVerTodo);
+        verTodo.setOnClickListener(v -> {
+            Fragment buscar = new BuscarFragmentUsuario();
+            requireActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content_container, buscar)   // host del activity
+                    .addToBackStack("buscar")                  // para volver con back
+                    .commit();
+        });
     }
 }
