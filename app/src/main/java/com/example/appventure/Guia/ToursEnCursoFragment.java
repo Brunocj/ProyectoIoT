e205e76786f146d9b012b238aeca9900fa7c5525
JJVA07 <a20203266@pucp.edu.pe>
2025-09-15 00:10:38 -0500

    MAS vistas de guía

---
+++ app/src/main/java/com/example/appventure/Guia/ToursEnCursoFragment.java
@@ -0,0 +1,25 @@
+package com.example.appventure.Guia;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.appventure.R;
+
+public class ToursEnCursoFragment extends Fragment {
+
+    public ToursEnCursoFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tours_en_curso, container, false);
+    }
+}
