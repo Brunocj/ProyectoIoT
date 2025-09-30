a5e3c876366a389f751f73cdca4335a339dda3ae
Bases21 <saabad@pucp.edu.pe>
2025-09-29 19:19:35 -0500

    Corrección de errores.

--- app/src/main/java/com/example/appventure/AddCardActivity.java
+++ app/src/main/java/com/example/appventure/AddCardActivity.java
@@ -12,7 +12,7 @@ public class AddCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_add_card); // usa tu layout existente
 
-        // back en la toolbar del formulario (opcional)
+        // back en la toolbar del formulario (opcional
         MaterialToolbar tb = findViewById(R.id.toolBarAgregarTarjeta);
         if (tb != null) tb.setNavigationOnClickListener(v -> onBackPressed());
     }
